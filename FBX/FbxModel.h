96a65c85f000148ecb3e127cf46a9b653c8b5b9a
LC1B_15_takahashi_tsubaki <k021g1132@g.neec.ac.jp>
2023-04-28 16:22:45 +0900

    01_08 の資料の最後まで完成

--- FBX/FbxModel.h
+++ FBX/FbxModel.h
@@ -4,6 +4,8 @@
 #include <DirectXMath.h>
 #include <vector>
 
+#include<DirectXTex.h>
+
 struct Node
 {
 	//名前
@@ -30,6 +32,14 @@ public:
 	//フレンドクラス
 	friend class FbxLoader;
 
+public://サブクラス
+	struct VertexPosNormalUv
+	{
+		DirectX::XMFLOAT3 pos;//xyz座標
+		DirectX::XMFLOAT3 normal;//法線ベクトル
+		DirectX::XMFLOAT3 uv;//uv座標
+	};
+
 private:
 
 	//モデル名
@@ -37,5 +47,21 @@ private:
 
 	//ノード配列
 	std::vector<Node> nodes;
+
+	//メッシュを持つノード
+	Node* meshNode = nullptr;
+	//頂点データ配列
+	std::vector<VertexPosNormalUv> vertices;
+	//インデックス配列
+	std::vector<unsigned short> indices;
+
+	//アンビエント係数
+	DirectX::XMFLOAT3 ambient = {1,1,1};
+	//ディフューズ係数
+	DirectX::XMFLOAT3 diffuse = { 1,1,1 };
+	//テクスチャメタデータ
+	DirectX::TexMetadata metadata = {};
+	//スクラッチイメージ
+	DirectX::ScratchImage scratchImg = {};
 
 };
