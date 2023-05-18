2145323a541dfca55d54afe473d8b3c52f1c7702
LC1B_15_takahashi_tsubaki <k021g1132@g.neec.ac.jp>
2023-05-18 13:07:17 +0900

    02_03 自作のモデルででけた

--- Resources/shaders/FBXVS.hlsl
+++ Resources/shaders/FBXVS.hlsl
@@ -17,3 +17,50 @@ VSOutput main(VSInput input)
 
 	return output;
 }
+//スキニング後の頂点、法線が入る
+struct SkinOutput
+{
+	float4 pos;
+	float3 normal;
+};
+
+SkinOutput ComputeSkin(VSInput input)
+{
+	//ゼロクリア
+	SkinOutput output = (SkinOutput)0;
+
+	uint iBone;//計算するボーン番号
+	float weight;//ボーンウェイト
+	matrix m;//スキニング行列
+
+	//ボーン0
+	iBone = input.boneIndices.x;
+	weight = input.boneWeights.x;
+	m = matSkinning[iBone];
+	output.pos += weight * mul(m, input.pos);
+	output.normal += weight * mul((float3x3)m,input.normal);
+
+	//ボーン1
+	iBone = input.boneIndices.y;
+	weight = input.boneWeights.y;
+	m = matSkinning[iBone];
+	output.pos += weight * mul(m, input.pos);
+	output.normal += weight * mul((float3x3)m, input.normal);
+
+	//ボーン2
+	iBone = input.boneIndices.z;
+	weight = input.boneWeights.z;
+	m = matSkinning[iBone];
+	output.pos += weight * mul(m, input.pos);
+	output.normal += weight * mul((float3x3)m, input.normal);
+
+	//ボーン3
+	iBone = input.boneIndices.w;
+	weight = input.boneWeights.w;
+	m = matSkinning[iBone];
+	output.pos += weight * mul(m, input.pos);
+	output.normal += weight * mul((float3x3)m, input.normal);
+
+	return output;
+
+}
