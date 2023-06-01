85fce53d2815be9562894ee6fe3aa08697e5584f
LC1B_15_takahashi_tsubaki <k021g1132@g.neec.ac.jp>
2023-06-01 12:40:48 +0900

    03_08 マルチテクスチャの完了

--- Resources/shaders/PostEffectTestPS.hlsl
+++ Resources/shaders/PostEffectTestPS.hlsl
@@ -1,6 +1,7 @@
 #include "PostEffectTest.hlsli"
 
-Texture2D<float4> tex : register(t0);	//0番スロットに設定されたテクスチャ
+Texture2D<float4> tex0 : register(t0);	//0番スロットに設定されたテクスチャ
+Texture2D<float4> tex1 : register(t1);	//1番スロットに設定されたテクスチャ
 SamplerState smp : register(s0);		//0番スロットに設定されたサンプラー
 
 
@@ -9,8 +10,15 @@ float4 main(VSOutput input) : SV_TARGET
 	float2 add = { 0.5f,0.0f };
 
 
-	float4 texcolor = tex.Sample(smp,input.uv+ add);
+	float4 texcolor0 = tex0.Sample(smp,input.uv);
+	float4 texcolor1 = tex1.Sample(smp, input.uv);
 
-	return float4(1.0f - texcolor.rgb,1);
+	float4 color = texcolor0;
+	if (fmod(input.uv.y,0.1f)<0.05f)
+	{
+		color = texcolor1;
+	}
+
+	return float4(color.rgb,1);
 
 }
